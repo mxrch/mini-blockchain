ad8f9c61464e2b22e3d235e9fc5aed405346892b
mxrch <17338428+mxrch@users.noreply.github.com>
2019-03-29 02:44:25 +0100

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,11 @@
+from blockchain import Blockchain
+
+block_one_transactions = {"sender":"Alice", "receiver": "Bob", "amount":"50"}
+block_two_transactions = {"sender": "Bob", "receiver":"Cole", "amount":"25"}
+block_three_transactions = {"sender":"Alice", "receiver":"Cole", "amount":"35"}
+
+local_blockchain = Blockchain()
+local_blockchain.add_block(block_one_transactions)
+local_blockchain.add_block(block_two_transactions)
+local_blockchain.add_block(block_three_transactions)
+local_blockchain.print_blocks()
